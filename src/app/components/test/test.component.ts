c9cde47564b2238074d43e0c3b3eb676eaca8ec6
Heybet Quliyev (TD/PCcheybetS Tests) <ccheybet@azercell.com>
2018-10-12 12:40:15 +0400

    bug fix

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { trigger, transition, useAnimation } from "@angular/animations";
+import { swing } from "ng-animate";
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.scss"],
+  animations: [trigger("swing", [transition("* => *", useAnimation(swing))])]
 })
 export class TestComponent implements OnInit {
+  swing: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
